61825283389151e7666332282bd232b75b42a818
Joseph Cobhams <tech.supreme1@gmail.com>
2018-06-22 11:18:13 +0100

    Created Migrations for Riders Model

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate, MigrateCommand
 from app import create_app
 from app.utils import db
 from config import get_env
+from app.models import rider
 
 app = create_app(get_env('APP_ENV'))
 migrate = Migrate(app, db)
